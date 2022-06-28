eb86b021fcf3a75b414ef975aaf6a4584faa50f6
@Azizjon <azizjon990415@gmail.com>
2022-06-28 18:41:45 +0500

    js

---
+++ lesson19/js/index.js
@@ -0,0 +1,6 @@
+function addTwoNumber(){
+    let firstNumber=document.getElementById("firstNumber").value
+    let secondNumber=document.getElementById("secondNumber").value
+    let solution = document.getElementById("solution")
+    solution.innerHTML="Solution:"+(firstNumber+secondNumber)
+}
